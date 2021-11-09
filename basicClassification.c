78a45cc00eb422462af9a06ed4a315913f7f723c
Ofir Rubin <45829637+ofirrubin@users.noreply.github.com>
2021-11-09 23:16:44 +0200

    fixed for n = 1
    It appears that you count 1 as prime number. fixed.

--- basicClassification.c
+++ basicClassification.c
@@ -9,8 +9,8 @@ int fact(int n){
 int isPrime(int n){ // Returns 1 if a number is a prime number, 0 otherwise.
     // A prime number is a number which is divisible only by 1 and itself.
     // We can check up to the sqrt of the number as the (sqrt)^2 is the number it's self thus every multiplier should've been on the lower part of the graph.
-    if (n < 2) return 0;
-    if (n == 2) return 1; // 2 is the only even prime number
+    if (n < 1) return 0;
+    if (n == 2 || n == 1) return 1; // 2 is the only even prime number, it appears that you count 1 as prime although it isn't.
     if (n % 2 == 0) return 0; // Every even number that is not 2 is not a prime number.
     int max = floor(sqrt((double)n));
     for (int i = 3; i <= max; i ++){
